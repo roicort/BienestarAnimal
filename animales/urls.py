4b33065580e9086e7be3326c27fb7e0afbc2a19e
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2023-12-07 17:48:23 -0600

    Fix Urls

--- animales/urls.py
+++ animales/urls.py
@@ -1,15 +1,16 @@
 from apirest.urls import drf_router
-from .viewsets import AnimalCategoriaViewSet, AnimalCaracteristicaViewSet, AdopcionViewSet, AnimalViewSet, ReportePerdidoViewSet, PostulacionAdopcionViewSet, MisPostulacionesAnimalViewSet, AnimalFavoritoViewSet
+from .viewsets import AnimalCategoriaViewSet, AnimalCaracteristicaViewSet, AdopcionViewSet, AnimalViewSet, ReportePerdidoViewSet, PostulacionAdopcionViewSet, MisPostulacionesAnimalViewSet, AnimalFavoritoViewSet, ReporteCiudadanoPerdidoViewSet
 
 urlpatterns = []
 
-drf_router.register(r'animales/padron', AnimalViewSet, basename='nimales-animal')
-drf_router.register(r'animales/adopciones', AdopcionViewSet, basename='adopciones-animal')
-drf_router.register(r'animales/reportes', ReportePerdidoViewSet, basename='empleos-reporte-perdido')
-drf_router.register(r'animales/postulacion-adopcion', PostulacionAdopcionViewSet, basename='empleos-postulacion-animal')
+drf_router.register(r'animales/padron', AnimalViewSet, basename='animales-animal')
+drf_router.register(r'animales/adopciones', AdopcionViewSet, basename='animales-adopciones-animal')
+drf_router.register(r'animales/reportes', ReportePerdidoViewSet, basename='animales-reporte-perdido')
+drf_router.register(r'animales/reportes-ciudadanos', ReporteCiudadanoPerdidoViewSet, basename='animales-reporte-perdido')
+drf_router.register(r'animales/postulacion-adopcion', PostulacionAdopcionViewSet, basename='animales-postulacion-animal')
 drf_router.register(r'animales/mis-postulaciones', MisPostulacionesAnimalViewSet, basename='mis-postulacion-animal')
-drf_router.register(r'animales/animal-favorito', AnimalFavoritoViewSet, basename='empleos-animal-favorita')
-drf_router.register(r'animales/animal-categoria', AnimalCategoriaViewSet, basename='empleos-animal-categoria')
-drf_router.register(r'animales/animal-inclusion', AnimalCaracteristicaViewSet, basename='empleos-animal-inclusion')
+drf_router.register(r'animales/animal-favorito', AnimalFavoritoViewSet, basename='animales-animal-favorita')
+drf_router.register(r'animales/animal-categoria', AnimalCategoriaViewSet, basename='animales-animal-categoria')
+drf_router.register(r'animales/animal-inclusion', AnimalCaracteristicaViewSet, basename='animales-animal-inclusion')
 
 urlpatterns += drf_router.urls
