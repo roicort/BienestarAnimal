7c2942d2384b9df5a5fe875af75df711c4737272
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2023-12-11 12:00:53 -0600

    Production

--- MTY_PIBA/wsgi.py
+++ MTY_PIBA/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "MTY_PIBA.settings.dev"
+os.environ["DJANGO_SETTINGS_MODULE"] = "MTY_PIBA.settings.production"
 
 application = get_wsgi_application()
