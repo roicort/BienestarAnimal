ab1b542c91342e9b5f1bbf0f14e33a3aac12f7aa
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2023-12-11 01:29:25 -0600

    Settings

--- MTY_PIBA/settings/production.py
+++ MTY_PIBA/settings/production.py
@@ -2,26 +2,26 @@ from .base import *
 
 DEBUG = False
 
-SECRET_KEY = "django-insecure-!q*mt*uab7p=rmo4zpk%uz9*o7ux#ayp+%&x)fp6kq(734lp^m"
+SECRET_KEY = "django!q*mt*uab7p=rmo4zpk%uz9*o7ux#ayp+%&x)fp6kq(734lp^m"
 
 ALLOWED_HOSTS = [
-    "adopta.drf.dev.mun.apismty.gob.mx"
+    "piba.drf.mun.apismty.gob.mx"
     ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://adoptamos.web.app",
-    "adoptamos.web.app",
+    "https://mascotas.web.app",
+    "mascotas.web.app",
     ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://adopta.drf.dev.mun.apismty.gob.mx",
+    "https://piba.drf.mun.apismty.gob.mx",
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'BTS API',
-    'DESCRIPTION': 'Bolsa de Empleo de Monterrey',
+    'TITLE': 'PIBA API',
+    'DESCRIPTION': 'Plafaforma de Bienestar Animal de Monterrey',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
